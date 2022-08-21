8b3276afe36dbc7be9ad12148a17042a40481573
yuka1324 <yu_ka092701@me.com>
2022-08-21 15:13:49 +0900

    ソースコード可読性向上のため、リフォーマット/変数、クラス名の変更

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragment.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright © 2021 YUMEMI Inc. All rights reserved.
+ */
+package jp.co.yumemi.android.code_check
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.*
+import jp.co.yumemi.android.code_check.databinding.SearchFragmentBinding
+
+class SearchFragment : Fragment(R.layout.search_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = SearchFragmentBinding.bind(view)
+        val viewModel = SearchViewModel(context!!)
+        val layoutManager = LinearLayoutManager(context!!)
+        val dividerItemDecoration = DividerItemDecoration(context!!, layoutManager.orientation)
+        val adapter = CustomAdapter(object : CustomAdapter.OnItemClickListener {
+            override fun itemClick(item: SearchResultContents) {
+                toDetailPage(item)
+            }
+        })
+
+        binding.searchInputText
+            .setOnEditorActionListener { editText, action, _ ->
+                if (action == EditorInfo.IME_ACTION_SEARCH) {
+                    editText.text.toString().let {
+                        viewModel.searchResults(it).apply {
+                            adapter.submitList(this)
+                        }
+                    }
+                    return@setOnEditorActionListener true
+                }
+                return@setOnEditorActionListener false
+            }
+
+        binding.recyclerView.also {
+            it.layoutManager = layoutManager
+            it.addItemDecoration(dividerItemDecoration)
+            it.adapter = adapter
+        }
+    }
+
+    fun toDetailPage(item: SearchResultContents) {
+        val action = SearchFragmentDirections
+            .actionToDetailPage(item)
+        findNavController().navigate(action)
+    }
+}
+
+val diffUtil = object : DiffUtil.ItemCallback<SearchResultContents>() {
+    override fun areItemsTheSame(
+        oldItem: SearchResultContents,
+        newItem: SearchResultContents
+    ): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(
+        oldItem: SearchResultContents,
+        newItem: SearchResultContents
+    ): Boolean {
+        return oldItem == newItem
+    }
+
+}
+
+class CustomAdapter(
+    private val itemClickListener: OnItemClickListener,
+) : ListAdapter<SearchResultContents, CustomAdapter.ViewHolder>(diffUtil) {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    interface OnItemClickListener {
+        fun itemClick(item: SearchResultContents)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        (holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView).text =
+            item.name
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.itemClick(item)
+        }
+    }
+}
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchViewModel.kt
@@ -19,15 +19,11 @@ import kotlinx.parcelize.Parcelize
 import org.json.JSONObject
 import java.util.*
 
-/**
- * TwoFragment で使う
- */
-class OneViewModel(
+class SearchViewModel(
     val context: Context
 ) : ViewModel() {
 
-    // 検索結果
-    fun searchResults(inputText: String): List<item> = runBlocking {
+    fun searchResults(inputText: String): List<SearchResultContents> = runBlocking {
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
@@ -37,14 +33,9 @@ class OneViewModel(
             }
 
             val jsonBody = JSONObject(response.receive<String>())
-
             val jsonItems = jsonBody.optJSONArray("items")!!
+            val searchResultList = mutableListOf<SearchResultContents>()
 
-            val items = mutableListOf<item>()
-
-            /**
-             * アイテムの個数分ループする
-             */
             for (i in 0 until jsonItems.length()) {
                 val jsonItem = jsonItems.optJSONObject(i)!!
                 val name = jsonItem.optString("full_name")
@@ -52,11 +43,11 @@ class OneViewModel(
                 val language = jsonItem.optString("language")
                 val stargazersCount = jsonItem.optLong("stargazers_count")
                 val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
+                val forksCount = jsonItem.optLong("forks_count")
                 val openIssuesCount = jsonItem.optLong("open_issues_count")
 
-                items.add(
-                    item(
+                searchResultList.add(
+                    SearchResultContents(
                         name = name,
                         ownerIconUrl = ownerIconUrl,
                         language = context.getString(R.string.written_language, language),
@@ -70,13 +61,13 @@ class OneViewModel(
 
             lastSearchDate = Date()
 
-            return@async items.toList()
+            return@async searchResultList.toList()
         }.await()
     }
 }
 
 @Parcelize
-data class item(
+data class SearchResultContents(
     val name: String,
     val ownerIconUrl: String,
     val language: String,
